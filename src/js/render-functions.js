174653293d7dff2905e67669a3fd035a37c8542a
TOR10S <andriivoro@gmail.com>
2024-07-24 15:14:38 +0300

    try 1.3

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,13 +4,13 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 export function createImages(data) {
-    const lightbox = new SimpleLightbox('.gallery a', { 
+    const lightbox = new SimpleLightbox('.gallery-list a', { 
         captions: true,
         captionsData: 'alt',
         captionDelay: 250
      });
     const list = document.querySelector(".gallery-list");
-    let images =  data.hits.map((hit) => `<div class="image-frame"><a src="${hit.largeImageURL}"><img class="image" src="${hit.webformatURL}" alt="${hit.tags}" /></a><h5>likes</h5><p>${hit.likes}</p></div>`).join("");
+    let images =  data.hits.map((hit) => `<div class="image-frame"><a href="${hit.largeImageURL}"><img class="image" src="${hit.webformatURL}" alt="${hit.tags}" /></a><h5>likes</h5><p>${hit.likes}</p></div>`).join("");
     list.insertAdjacentHTML("afterbegin", images);
     lightbox.refresh();
 }
